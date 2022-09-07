fefcf986a917fa5c8438efae648cc1f3f042c385
Josh <95011683+jopo86@users.noreply.github.com>
2022-09-06 21:07:19 -0400

    Add files via upload

---
+++ src/Shader.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <glad/glad.h>
+
+class Shader {
+public:
+    Shader(const char* v_path, const char* f_path);
+
+    void bind();
+    void unbind();
+
+    void uniform_mat4(const char* name, const GLfloat* val);
+    void uniform_vec3(const char* name, const GLfloat* val);
+    void uniform_float(const char* name, const GLfloat val);
+
+    void clear();
+    ~Shader();
+
+private:
+    GLuint program;
+    const char* vs;
+    const char* fs;
+
+    void compile();
+    void add(const char* code, GLenum type);
+    std::string read_file(const char* path);
+};
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "Application.hpp"
+
+int main() {
+	
+	Application app;
+	app.init();
+	app.run();
+
+	return 0;
+}
